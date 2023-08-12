643112986e878ca219cfb99e6542e45109795137
ICEFIR <icefir@gmail.com>
2023-08-13 03:17:42 +1000

    Revamped syntax

--- src/lib.rs
+++ src/lib.rs
@@ -4,107 +4,109 @@
 /// use error_ex::{create_error};
 /// create_error!(ErrorType => ErrorReason1, ErrorReason2, ErrorReason3);
 /// ```
-/// # Examples
+/// ## Examples
 ///
 /// ```
-/// use error_ex::{create_error, map_to_error};
+/// use error_ex::{create_error};
 ///
 /// create_error!(InputError => IllegalArgument, InvalidInput, MissingArgument);
 /// //Now, you can use the following code to instantiate this error
-/// InputError::IllegalArgument(format!("Your message here"));
+/// InputError::illegal_argument(format!("Your message here"));
+///
+/// ```
+///
+/// ## Error Mapping
+///
+/// The explicit way
+/// ```
+/// use std::fs;
+/// use error_ex::{create_error};
+/// create_error!(FileError => IoError);
+/// create_error!(SchemaError => ParseError);
+/// let error: Result<(), FileError::Error> = Err(FileError::io_error("".to_string()));
+/// let mapped = error.map_err(|error| {
+///     SchemaError::parse_error(format!("SchemaError::ParseError error {error}"))
+/// });
+///
+/// ```
+/// ## Function wrapper
+/// The above code can be simplified using `map_to_error!`
+/// macro using the build in lower order function
+/// ```
+///
+/// use std::fs;
+/// use std::io::Error;
+/// use error_ex::{create_error};
+/// create_error!(FileError => IoError);
+/// create_error!(SchemaError => ParseError);
+/// let error: Result<(), FileError::Error> = Err(FileError::IoError("".to_string()));
+/// let mapped = error.map_err(SchemaError::map_parse_error);
 ///
 /// ```
 ///
 #[macro_export]
 macro_rules! create_error {
     ( $error:ident => $( $error_reason:ident ),* ) => {
-        #[allow(non_snake_case)]
-        pub mod $error {
-            pub use paste::paste;
-            pub use std::fmt;
+        paste::paste! {
 
-            paste! {
-                #[derive(Debug, Clone, PartialEq, Eq)]
-                pub struct Error {
-                    pub reason: Reason,
-                    pub message: String,
-                }
+            #[derive(Debug, Clone, PartialEq, Eq)]
+            pub struct $error {
+                pub reason: [<$error Reason>],
+                pub message: String,
+            }
 
-                #[derive(Debug, Clone, PartialEq, Eq)]
-                #[allow(unused_qualifications)]
-                pub enum Reason {
-                    $(
-                        [<$error_reason>],
-                    )*
-                }
+            #[derive(Debug, Clone, PartialEq, Eq)]
+            #[allow(unused_qualifications)]
+            pub enum [<$error Reason>] {
+                $(
+                    [<$error_reason>],
+                )*
+            }
 
+            impl $error {
                 $(
                     #[allow(unused_qualifications)]
-                    pub fn [<$error_reason>](message: String) -> Error {
-                        Error {
-                            reason: Reason::$error_reason,
+                    pub fn [<$error_reason:snake>](message: String) -> $error {
+                        $error {
+                            reason: [<$error Reason>]::$error_reason,
                             message,
                         }
                     }
                 )*
 
-                impl std::error::Error for Error {}
-
-                impl fmt::Display for Error {
-                    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-                        let error_name = stringify!([<$error:camel>]);
-                        write!(
-                            f,
-                            "{:#?} error, reason: {:#?}, message {:#?}",
-                            error_name,
-                            self.reason,
-                            self.message
-                        )
+                $(
+                    #[allow(unused_qualifications)]
+                    pub fn [<map_ $error_reason:snake>]<E>(error: E) -> $error
+                    where
+                        E: std::error::Error,
+                    {
+                        let error_name = stringify!($error);
+                        let error_reason_name = stringify!($error_reason);
+                        let error_string =
+                            format!("{}::{} caused by {}", error_name, error_reason_name, error);
+                        $error {
+                            reason: [<$error Reason>]::$error_reason,
+                            message: error_string,
+                        }
                     }
+                )*
+            }
+
+
+            impl std::error::Error for $error {}
+
+            impl std::fmt::Display for $error {
+                fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+                    let error_name = stringify!([<$error:camel>]);
+                    write!(
+                        f,
+                        "{:#?} error, reason: {:#?}, message {:#?}",
+                        error_name,
+                        self.reason,
+                        self.message
+                    )
                 }
             }
-        }
-    };
-}
-
-/// Helper for mapping errors from one to another
-/// # Usage
-/// ### Error Mapping
-///
-/// The explicit way
-/// ```
-/// use std::fs;
-/// use error_ex::{create_error, map_to_error};
-/// create_error!(FileError => IoError);
-/// create_error!(SchemaError => ParseError);
-/// let error: Result<(), FileError::Error> = Err(FileError::IoError("".to_string()));
-/// let mapped = error.map_err(|error| {
-///     SchemaError::ParseError(format!("SchemaError::ParseError error {error}"))
-/// });
-///
-/// ```
-/// ### Macro error mapping
-/// The above code can be simplified using `map_to_error!` macro
-/// ```
-///
-/// use std::fs;
-/// use std::io::Error;
-/// use error_ex::{create_error, map_to_error};
-/// create_error!(FileError => IoError);
-/// create_error!(SchemaError => ParseError);
-/// let error: Result<(), FileError::Error> = Err(FileError::IoError("".to_string()));
-/// let mapped = error.map_err(map_to_error!(SchemaError::ParseError));
-///
-/// ```
-///
-#[macro_export]
-macro_rules! map_to_error {
-    ($error:ident :: $error_reason:ident) => {
-        |error| {
-            let error_name = stringify!($error);
-            let error_reason_name = stringify!($error_reason);
-            let error_string = format!("{}::{} caused by {}", error_name, error_reason_name, error);
-            $error::$error_reason(error_string)
         }
     };
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use error_ex::{create_error, map_to_error};
+    use error_ex::create_error;
     // Given
     create_error!(InputError => IllegalArgument, InvalidInput, MissingArgument);
     create_error!(ResponseError => ParamError);
@@ -8,36 +8,36 @@ mod tests {
     #[test]
     fn should_able_to_construct_error() {
         // when
-        let invalid_input = InputError::InvalidInput(format!("Test"));
+        let invalid_input = InputError::invalid_input(format!("Test"));
+        let illegal_argument = InputError::illegal_argument(format!("Test 2"));
 
-        let illegal_argument = InputError::IllegalArgument(format!("Test 2"));
-
-        let missing_argument = InputError::Error {
-            reason: InputError::Reason::MissingArgument,
+        let missing_argument = InputError {
+            reason: InputErrorReason::MissingArgument,
             message: "Test 3".to_string(),
         };
 
         // then
-        assert_eq!(invalid_input.reason, InputError::Reason::InvalidInput);
+        assert_eq!(invalid_input.reason, InputErrorReason::InvalidInput);
         assert_eq!(invalid_input.message, "Test");
-        assert_eq!(illegal_argument.reason, InputError::Reason::IllegalArgument);
+        assert_eq!(illegal_argument.reason, InputErrorReason::IllegalArgument);
         assert_eq!(illegal_argument.message, "Test 2");
-        assert_eq!(missing_argument.reason, InputError::Reason::MissingArgument);
+        assert_eq!(missing_argument.reason, InputErrorReason::MissingArgument);
         assert_eq!(missing_argument.message, "Test 3");
     }
 
     #[test]
     fn should_able_to_map_an_error_to_another_error() {
         // given
-        let res: Result<(), InputError::Error> = Err(InputError::InvalidInput(format!("Test")));
+        let res: Result<(), InputError> = Err(InputError::invalid_input(format!("Test")));
 
         // when
-        let mapped_res = res.map_err(map_to_error!(ResponseError::ParamError));
+
+        let mapped_res = res.map_err(ResponseError::map_param_error);
 
         // then
         assert_eq!(mapped_res.is_err(), true);
         let error = mapped_res.unwrap_err();
-        assert_eq!(error.reason, ResponseError::Reason::ParamError);
+        assert_eq!(error.reason, ResponseErrorReason::ParamError);
         assert_eq!(
             error.message,
             format!(
